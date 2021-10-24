04bbc1d13614e40d391eabb6a95444e0ba536939
= <=>
2021-10-24 20:22:24 +0200

    addbook mutation, prevent duplicates from isbn or title.

--- src/schema/resolvers/mutation.ts
+++ src/schema/resolvers/mutation.ts
@@ -8,8 +8,22 @@ const mutationResolvers: IResolvers = {
         addBook: ( _: void, args: { book: IBook }): {
             status: boolean,
             message: string,
-            item: IBook
+            item?: IBook
         } => {
+
+            if ( data.books.filter( (value: IBook) => value.title.toLocaleLowerCase() === args.book.title.toLocaleLowerCase() ).length > 0 ) {
+                return {
+                    status: false,
+                    message: `Ya existe un libro con el título ${args.book.title}`
+                }
+            } 
+
+            if ( data.books.filter( (value: IBook) => value.isbn === args.book.isbn ).length > 0 ) {
+                return {
+                    status: false,
+                    message: `Ya existe un libro con el ISBN ${args.book.isbn}`
+                }
+            } 
 
             const idValue = +data.books[data.books.length-1].id + 1;
             args.book.id = String( idValue );
