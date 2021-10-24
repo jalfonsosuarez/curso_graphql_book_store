10279fc0ad933a63ac3e4a9ff7505e0ffbaa5d20
= <=>
2021-10-24 20:04:36 +0200

    Mutation addBook

--- src/schema/resolvers/mutation.ts
+++ src/schema/resolvers/mutation.ts
@@ -1,9 +1,25 @@
 import { IResolvers } from '@graphql-tools/utils';
+import { IBook } from '../../interfaces/book.interface';
+import data from '../../data/index';
 
 const mutationResolvers: IResolvers = {
 
     Mutation: {
-        // addBook( id: ID! ): Boolean
+        addBook: ( _: void, args: { book: IBook }): {
+            status: boolean,
+            message: string,
+            item: IBook
+        } => {
+
+            const idValue = +data.books[data.books.length-1].id + 1;
+            args.book.id = String( idValue );
+            (data.books as IBook[]).push(args.book);
+            return {
+                status: true,
+                message: 'Libro añadido',
+                item: args.book
+            };
+        } 
         // updateBook( id: ID! ): Boolean
         // deleteBook( id: ID! ): Boolean        
     }
