16f8a2ea00e7523e01e45412bb21c0ff20cd987e
= <=>
2021-10-24 21:43:33 +0200

    mutation from People ready.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import { makeExecutableSchema } from "@graphql-tools/schema";
-import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServer } from "apollo-server-express";
 import compression from "compression";
 import express, { Application } from "express";
 import { GraphQLSchema } from "graphql";
+import depthLimit from "graphql-depth-limit";
 import { createServer, Server } from "http";
-import schema from './schema/index';
+
 
 class GraphQLServer {
   // Propiedades
@@ -38,6 +38,7 @@ class GraphQLServer {
     const apolloServer = new ApolloServer({
       schema: this.schema,
       introspection: true,
+      validationRules: [ depthLimit(5) ]
     });
 
     await apolloServer.start();
